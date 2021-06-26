0e05ef2061fa83c3aab9cf3386a53942788e7c35
Dima Kostov <dimakostov@yahoo.com>
2021-06-26 18:41:59 -0400

    finished

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const commandLineArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
+const generatePage = require('./src/page-template');
 
-const printProfileData = (profileDataArr) => {
-    
-    profileDataArr.forEach(item => {
-        console.log(item);
-    });
-}
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(commandLineArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err=> {
+    if(err) throw new Error(err);
+
+    console.log("Portfolio complete! Check ou index.html to see the output!");
+})
